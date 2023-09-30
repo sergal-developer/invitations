76637e6c37e058e9f4220f9abc7e9e09127953a1
sergio.gallegos <sergio.gallegos@live.com.mx>
2023-09-30 04:02:20 -0600

    update page to astro

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    outDir: './docs',
+    site: 'https://sergal-developer.github.io/invitations/'
+});
