224a397fda1346a4159081e00ac211cda895a26c
sergio.gallegos <sergio.gallegos@live.com.mx>
2023-10-04 01:13:58 -0600

    UPge

--- public/data/main.js
+++ public/data/main.js
@@ -1,8 +1,72 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { tsParticles } from "tsparticles-engine";
+import Wow from 'wow.js';
 export class APP {
-    constructor() { }
+    constructor() {
+        this.configs = {
+            background: {
+                color: "#000" // this sets a background color for the canvas
+            },
+            fullScreen: {
+                enable: true,
+                zIndex: -1 // this is the z-index value used when the fullScreen is enabled, it's 0 by default
+            },
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push" // adds the particles on click
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse" // make the particles run away from the cursor
+                    }
+                },
+                modes: {
+                    push: {
+                        quantity: 10 // number of particles to add on click
+                    },
+                    repulse: {
+                        distance: 100 // distance of the particles from the cursor
+                    }
+                }
+            },
+            particles: {
+                links: {
+                    enable: true,
+                    distance: 200 // maximum distance for linking the particles
+                },
+                move: {
+                    enable: true,
+                    speed: { min: 1, max: 5 } // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
+                },
+                number: {
+                    value: 50 // number of particles
+                },
+                opacity: {
+                    value: { min: 0.3, max: 0.7 } // using a different opacity, to have some semitransparent effects
+                },
+                size: {
+                    value: { min: 1, max: 3 } // let's randomize the particles size a bit
+                }
+            }
+        };
+    }
     init() {
         //this.setupIntro();
-        this.setupEvents();
+        // this.setupEvents();
+        const wow = new Wow();
+        wow.init();
+        console.log("init");
+        // this.loadParticles(this.configs);
     }
     setupEvents() {
         this.paralaxInvitation();
@@ -65,5 +129,11 @@ export class APP {
         const clientHeight = document.documentElement.clientHeight;
         const percentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
         return percentage;
+    }
+    loadParticles(options) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // await loadFull(tsParticles);
+            yield tsParticles.load(options);
+        });
     }
 }
--- src/data/main.ts
+++ src/data/main.ts
@@ -1,8 +1,66 @@
+import { tsParticles } from "tsparticles-engine";
+import Wow from 'wow.js';
+
 export class APP {
+
+    configs = {
+        background: {
+            color: "#000" // this sets a background color for the canvas
+        },
+        fullScreen: {
+            enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
+            zIndex: -1 // this is the z-index value used when the fullScreen is enabled, it's 0 by default
+        },
+        interactivity: {
+            events: {
+                onClick: {
+                    enable: true, // enables the click event
+                    mode: "push" // adds the particles on click
+                },
+                onHover: {
+                    enable: true, // enables the hover event
+                    mode: "repulse" // make the particles run away from the cursor
+                }
+            },
+            modes: {
+                push: {
+                    quantity: 10 // number of particles to add on click
+                },
+                repulse: {
+                    distance: 100 // distance of the particles from the cursor
+                }
+            }
+        },
+        particles: {
+            links: {
+                enable: true, // enabling this will make particles linked together
+                distance: 200 // maximum distance for linking the particles
+            },
+            move: {
+                enable: true, // enabling this will make particles move in the canvas
+                speed: { min: 1, max: 5 } // using a range in speed value will make particles move in a random speed between min/max values, each particles have its own value, it won't change in time by default
+            },
+            number: {
+                value: 50 // number of particles
+            },
+            opacity: {
+                value: { min: 0.3, max: 0.7 } // using a different opacity, to have some semitransparent effects
+            },
+            size: {
+                value: { min: 1, max: 3 } // let's randomize the particles size a bit
+            }
+        }
+    };
+
     constructor() { }
     init() {
         //this.setupIntro();
-        this.setupEvents();
+        // this.setupEvents();
+        const wow = new Wow();
+        wow.init();
+        
+        console.log("init");
+        // this.loadParticles(this.configs);
     }
     setupEvents() {
         this.paralaxInvitation();
@@ -66,4 +124,14 @@ export class APP {
         const percentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
         return percentage;
     }
+
+    async loadParticles(options) {
+        // await loadFull(tsParticles);
+        await tsParticles.load(options);
+    }
 }
+
+
+
+
+
